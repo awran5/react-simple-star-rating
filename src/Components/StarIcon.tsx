cf246e53dcb9faf06929d59c2a4b45429bd2dec9
awran5 <awran5@yahoo.com>
2021-11-28 21:19:21 +0200

    feat: add: new props

--- src/Components/StarIcon.tsx
+++ src/Components/StarIcon.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
 
 interface StarIconProps {
-  size: number
+  size?: number
+  strokeColor?: string
+  storkeWidth?: number
+  className?: string
+  style?: React.CSSProperties
 }
 
-export default function StarIcon({ size = 25 }: StarIconProps): JSX.Element {
+export function StarIcon({
+  size = 25,
+  strokeColor = 'none',
+  storkeWidth = 0,
+  className = 'star-svg',
+  style
+}: StarIconProps) {
   return (
-    <svg fill='currentColor' width={size} height={size} viewBox='0 0 24 24'>
-      <path d='M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z' />
+    <svg fill='currentColor' width={size} height={size} viewBox='0 0 24 24' className={className} style={{ ...style }}>
+      <path
+        fill='currentColor'
+        stroke={strokeColor}
+        strokeMiterlimit='10'
+        strokeWidth={storkeWidth}
+        d='M12,17.27L18.18,21l-1.64-7.03L22,9.24l-7.19-0.61L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27z'
+      />
     </svg>
   )
 }
