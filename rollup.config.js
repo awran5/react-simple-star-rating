9acc4db17341c95f2669cf3371a5077e4b58776f
awran5 <awran5@yahoo.com>
2022-10-03 02:13:23 +0200

    feat: add postcss

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,33 @@
-import typescript from 'rollup-plugin-typescript2'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import { terser } from 'rollup-plugin-terser'
-import packageJson from './package.json'
+import typescript from "rollup-plugin-typescript2"
+import peerDepsExternal from "rollup-plugin-peer-deps-external"
+import resolve from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
+import postcss from "rollup-plugin-postcss"
+import autoprefixer from "autoprefixer"
+import { terser } from "rollup-plugin-terser"
+import packageJson from "./package.json"
 
-export default {
-  input: 'src/index.tsx',
-  plugins: [peerDepsExternal(), resolve(), commonjs(), typescript(), terser()],
+const config = {
+  input: "src/index.tsx",
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({
+      exclude: ["src/stories/**"]
+    }),
+    postcss({
+      modules: true,
+      extract: false,
+      minimize: true,
+      plugins: [autoprefixer()]
+    }),
+    terser()
+  ],
   output: {
     file: packageJson.main,
-    format: 'cjs',
+    format: "cjs",
     sourcemap: true
   }
 }
+export default config
